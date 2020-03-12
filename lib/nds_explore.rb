c47d6bce68ab432fcefdce983e3575fa8b695586
Sam Gorick <samgorick.sg@gmail.com>
2020-03-12 21:57:21 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,8 +12,8 @@ end
 def print_first_directors_movie_titles
   counter = 0
   steven = directors_database[0]
-  while counter < directors_database.length do
-    puts "#{directors_database[counter][:title]}"
+  while counter < steven.length do
+    puts "#{steven[counter][:title]}"
     counter +=1
   end
 end
