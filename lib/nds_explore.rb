05e12830f3bd14cda1f2bdca923e89926a7ac45d
Sam Gorick <samgorick.sg@gmail.com>
2020-03-12 21:55:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  counter = 0
+  while counter < directors_database.length do
+    puts "Film #{counter} is #{films[counter][:title]}"
+    counter +=1
+  end
 end
-
-pretty_print_nds(directors_database)
